785ae89e2ffa770885fecdc78cd44410989fb445
bill.chou <bill.chou@nogle.com>
2022-07-28 12:00:30 +0800

    fix redis config

--- src/main/java/com/bill/config/JedisConfig.java
+++ src/main/java/com/bill/config/JedisConfig.java
@@ -8,27 +8,16 @@ import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class JedisConfig {
-    @Value("${spring.redis.host}")
-    String host;
-    @Value("${spring.redis.port}")
-    int port;
-    @Value("${spring.redis.timeout}")
-    int time;
-    @Value("${spring.redis.jedis.pool.max-active}")
-    int poolTotal;
-    @Value("${spring.redis.jedis.pool.max-idle}")
-    int maxIdle;
-    @Value("${spring.redis.jedis.pool.min-idle}")
-    int minIdle;
     @Bean
-    public JedisPool JedisPoolUtil() {
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setTestOnBorrow(true);
-        poolConfig.setMaxTotal(poolTotal);
-        poolConfig.setMaxIdle(maxIdle);
-        poolConfig.setMinIdle(minIdle);
-        poolConfig.setMaxWaitMillis(time);
-        poolConfig.setTestWhileIdle(true);
-        return new JedisPool(poolConfig, host, port);
+    public JedisPoolConfig jedisPoolConfig() {
+        return new JedisPoolConfig();
+    }
+
+    @Bean
+    public JedisPool jedisPool(JedisPoolConfig config) {
+        config.setTestOnBorrow(true);
+        config.setTestWhileIdle(true);
+        return new JedisPool(config);
+
     }
 }
